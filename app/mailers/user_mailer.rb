36345715d40f650a0c2d80e1b210efe3a63045e6
Julien Da Silva <julien.da.silva@hotmail.com>
2017-03-13 17:32:34 +0100

    added attachment to mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,8 @@ class UserMailer < ApplicationMailer
 
   def certificate(user)
     @user = user  # Instance variable => available in view
-    mail.attachments['parcel.klm'] = File.join(Rails.root('app/assets/images/parcel.klm')
-    mail(to: @user.email, subject: 'Your certificate', parcel: parcel)
+    mail.attachments['parcel.klm'] = File.join("#{Rails.root}app/assets/images/parcel.klm")
+    mail(to: @user.email, subject: 'Your certificate')
      # This will render a view in `app/views/user_mailer`!
   end
 end
