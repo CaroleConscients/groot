fce13a312d33b0d4fadbffe6f994575417a33992
Carole Piette <carole@conscients.com>
2017-03-13 11:48:11 +0100

    omniauth controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.find_for_twitter_oauth(request.env['omniauth.auth'])
 
     if user.persisted?
-      flash.notice = "Vous avez planté groot #{User.count}! "
+      flash.notice = "Vous avez dejà planté groot #{User.count}! "
       sign_in_and_redirect user
     else
       flash.notice = "Oups, votre authentifaction twitter a échoué."
