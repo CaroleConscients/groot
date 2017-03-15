0b981490dac91bfb24a61085cedfb9a4aa99e7ba
Julien Da Silva <julien.da.silva@hotmail.com>
2017-03-15 12:48:53 +0100

    flash message for user account already existing

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.find_for_twitter_oauth(request.env['omniauth.auth'])
 
     if user.persisted?
-      unless user.card.last_turned
+      unless user.congratulation
         flash.notice = "Vous avez déjà planté le groot #{user.card.tree_id}!"
       end
       sign_in_and_redirect user
