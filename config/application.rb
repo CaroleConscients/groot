e072e46fdd83915eb610508dd6969845ce56395a
Julien Da Silva <julien.da.silva@hotmail.com>
2017-03-15 10:57:15 +0100

    setup sidekiq

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module Groot
       generate.assets false
     end
 
+    # Use sidekiq for active job
+    config.active_job.queue_adapter = :sidekiq
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
