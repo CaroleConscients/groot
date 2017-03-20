501b3d20aab4f03a888c41ad1123ba46b6aff59e
Carole Piette <carole@conscients.com>
2017-03-20 15:47:56 +0100

    2 thousands identical images from poster + cache one color

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ Card.destroy_all
 
 colors = %w(#F2F2F0 #11BFAE #456173 #1B3C59)
 
-for i in 1..1000
-  picture = File.open(File.join(Rails.root,"app/assets/images/final_groot/_#{i}.png"))
+for i in 1..2000
+  picture = File.open(File.join(Rails.root,"app/assets/images/final_groot/_#{i}.jpg"))
   Card.create(color: colors.sample, picture: picture, turned: false)
   puts "image #{i} created"
 end
