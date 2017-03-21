fdcbcb15ff477085be6f9b8a5658fe5f825b51af
Julien Da Silva <julien.da.silva@hotmail.com>
2017-03-21 11:17:20 +0100

    starting the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ colors = %w(#F2F2F0 #11BFAE #456173 #1B3C59)
 # end
 
 for i in 1..2000
-picture = File.open(File.join(Rails.root,"app/assets/images/final_groot/_#{i}.jpg"))
-Card.create(color: colors.sample, picture: picture, turned: false)
-puts "image #{i} created"
+  picture = File.open(File.join(Rails.root,"app/assets/images/final_groot/_#{i}.jpg"))
+  Card.create(color: colors.sample, picture: picture, turned: false)
+  puts "image #{i} created"
 end
