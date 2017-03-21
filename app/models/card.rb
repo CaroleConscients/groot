e0653e04cef9056ef3bc3235f8b05d28191f5476
Julien Da Silva <julien.da.silva@hotmail.com>
2017-03-21 16:16:09 +0100

    trying to precompile assets

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,7 +3,7 @@ class Card < ApplicationRecord
   belongs_to :user, optional: true
 
   #Paperclip configuration
-  has_attached_file :picture
+  has_attached_file :picture, styles: { thumb: "20x23>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 
   validates :picture, attachment_presence: true
